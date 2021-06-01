b8fb4ddbb947c122a3a93d4ccc190de302df217f
Tristan Protzman <tlprotzman@gmail.com>
2021-06-01 12:57:14 -0400

    phi and eta scale/resolution

--- macro/plotJetAngularResolution.cpp
+++ macro/plotJetAngularResolution.cpp
@@ -21,23 +21,23 @@
 
 // Hist Binning Parameters
 const int bins_1d = 150;
-const int bins_2d = 400;
+const int bins_2d = 100;
 const int bin_resolution = 15;
 const double phiRange = TMath::Pi() + 0.1;
-const double truthEtaRange = 4.5;
-const double recoEtaRange = 1.7;
+const double truthEtaRange = 5;
+const double recoEtaRange = 5;
 
 
 const double phiMin = -1 * phiRange;
 const double phiMax = phiRange;
-const double truthEtaMin = -1 * truthEtaRange;
-const double truthEtaMax = truthEtaRange;
-const double recoEtaMin = -1 * recoEtaRange;
-const double recoEtaMax = recoEtaRange;
+const double truthEtaMin = -1.5;//-1 * truthEtaRange;
+const double truthEtaMax = 4;//truthEtaRange;
+const double recoEtaMin = -1.5;//-1 * recoEtaRange;
+const double recoEtaMax = 4;//recoEtaRange;
 
 
 float calculateAngularity(float eta, float pt, float e, float parameter=-2.0) {
-    
+    return 0;
 }
 
 void plotJetAngularResolution(std::string inFilePath = "smallfilelist.txt") {
@@ -54,6 +54,8 @@ void plotJetAngularResolution(std::string inFilePath = "smallfilelist.txt") {
     TH2D *phiHist2d = new TH2D("phi, truth->reco", "", bins_2d, phiMin, phiMax, bins_2d, phiMin, phiMax);
     TH2D *etaHist2d = new TH2D("eta, truth->reco", "", bins_2d, truthEtaMin, truthEtaMax, bins_2d, recoEtaMin, recoEtaMax);
     TH2D *normalizedEtaHist = new TH2D("eta, (reco-truth)/truth", "", bin_resolution, recoEtaMin, recoEtaMax, bin_resolution, recoEtaMin, recoEtaMax);
+    TH2D *normalizedPhiHist = new TH2D("phi, (reco-truth)/truth", "", bin_resolution, phiMin, phiMax, bin_resolution, phiMin, phiMax);
+    
     
     TH1D *gphiHist = new TH1D("gphi", "", bins_1d, phiMin, phiMax);
     TH1D *phiHist = new TH1D("phi", "", bins_1d, phiMin, phiMax);
@@ -83,17 +85,25 @@ void plotJetAngularResolution(std::string inFilePath = "smallfilelist.txt") {
             float dEta, dPhi;
             dEta = truthEta - recoEta;
             dPhi = truthPhi - recoPhi;
+            if (dPhi > TMath::Pi()) {
+                dPhi -= TMath::TwoPi();
+            }
+            if (dPhi < -1 * TMath::Pi()) {
+                dPhi += TMath::TwoPi();
+            }
             if (0.4 * 0.4 < dEta * dEta + dPhi * dPhi) {
                 continue;
             }
-            if (abs(truthEta) > 1.5) {
-                continue;
+            // if (abs(truthEta) > 1.5) {
+            //     continue;
+            // }
+            if (!std::isnan(truthPhi) && !std::isnan(recoPhi)) {
+                phiHist2d->Fill(truthPhi, recoPhi);
+                normalizedPhiHist->Fill(truthPhi, (recoPhi - truthPhi));
             }
-            if (!std::isnan(truthPhi) && !std::isnan(recoPhi)) {phiHist2d->Fill(truthPhi, recoPhi);}
             if (!std::isnan(truthEta) && !std::isnan(recoEta))   {
                 etaHist2d->Fill(truthEta, recoEta);
-                float truthAngularity, recoAngularity;
-                normalizedEtaHist->Fill(truthEta, (recoEta - truthEta) / truthEta);
+                normalizedEtaHist->Fill(truthEta, (recoEta - truthEta));
             }
             gphiHist->Fill(truthPhi);
             phiHist->Fill(recoPhi);
@@ -108,21 +118,38 @@ void plotJetAngularResolution(std::string inFilePath = "smallfilelist.txt") {
 
 
     // Calculate scale and resolution of the jet angularity measurement 
-    TProfile *profile = normalizedEtaHist->ProfileX();
-    TH1D *projection = normalizedEtaHist->ProjectionX();
-    profile->BuildOptions(0, 0, "s");
+    TProfile *etaProfile = normalizedEtaHist->ProfileX();
+    TH1D *etaProjection = normalizedEtaHist->ProjectionX();
+    etaProfile->BuildOptions(0, 0, "s");
     double *eta = (double*)malloc(bin_resolution * sizeof(double));
-    double *scale = (double*)malloc(bin_resolution * sizeof(double));
-    double *resolution = (double*)malloc(bin_resolution * sizeof(double));
-    int fullBins = 0;
+    double *etaScale = (double*)malloc(bin_resolution * sizeof(double));
+    double *etaResolution = (double*)malloc(bin_resolution * sizeof(double));
+    int fullEtaBins = 0;
     for (uint32_t i = 1; i <= bin_resolution; i++) {
-        if (projection->GetBinContent(i) == 0) {
+        if (etaProjection->GetBinContent(i) == 0) {
             continue;
         }
-        eta[fullBins] = profile->GetBinCenter(i);
-        scale[fullBins] = profile->GetBinContent(i);
-        resolution[fullBins] = profile->GetBinError(i);
-        fullBins++;
+        eta[fullEtaBins] = etaProfile->GetBinCenter(i);
+        etaScale[fullEtaBins] = etaProfile->GetBinContent(i);
+        etaResolution[fullEtaBins] = etaProfile->GetBinError(i);
+        fullEtaBins++;
+    }
+    
+    TProfile *phiProfile = normalizedPhiHist->ProfileX();
+    TH1D *phiProjection = normalizedPhiHist->ProjectionX();
+    phiProfile->BuildOptions(0, 0, "s");
+    double *phi = (double*)malloc(bin_resolution * sizeof(double));
+    double *phiScale = (double*)malloc(bin_resolution * sizeof(double));
+    double *phiResolution = (double*)malloc(bin_resolution * sizeof(double));
+    int fullPhiBins = 0;
+    for (uint32_t i = 1; i <= bin_resolution; i++) {
+        if (phiProjection->GetBinContent(i) == 0) {
+            continue;
+        }
+        phi[fullPhiBins] = phiProfile->GetBinCenter(i);
+        phiScale[fullPhiBins] = phiProfile->GetBinContent(i);
+        phiResolution[fullPhiBins] = phiProfile->GetBinError(i);
+        fullPhiBins++;
     }
 
 
@@ -140,14 +167,18 @@ void plotJetAngularResolution(std::string inFilePath = "smallfilelist.txt") {
     etaHist2d->Draw("colz");
     etaHist2d->SetXTitle("truth eta");
     etaHist2d->SetYTitle("reco eta");
-    etaHist2d->SetTitle("eta, 10 GeV x 100 GeV, truth->reco");
+    etaHist2d->SetTitle("eta, 10x100 GeV");
     gPad->SetLogz();
     
 
     jetEnergy->cd(2);
-    normalizedEtaHist->Draw("colz");
-    normalizedEtaHist->SetXTitle("truth eta");
-    normalizedEtaHist->SetYTitle("(reco - truth) / truth eta");
+    // normalizedEtaHist->Draw("colz");
+    // normalizedEtaHist->SetXTitle("truth eta");
+    // normalizedEtaHist->SetYTitle("(reco - truth) / truth eta");
+    phiHist2d->Draw("colz");
+    phiHist2d->SetXTitle("truth phi");
+    phiHist2d->SetXTitle("reco phi");
+    phiHist2d->SetTitle("phi, 10x100 GeV");
     gPad->SetLogz();
     
 
@@ -191,41 +222,70 @@ void plotJetAngularResolution(std::string inFilePath = "smallfilelist.txt") {
     }
     if (true) {
         jetEnergy->cd(3);
-        profile->Draw("");
-        profile->SetXTitle("Eta");
+        TGraph *etaScaleGraph = new TGraph(fullEtaBins, eta, etaScale);
+        etaScaleGraph->GetXaxis()->SetTitle("Eta");
+        etaScaleGraph->GetYaxis()->SetTitle("Scale (Mean((reco-truth)/truth))");
+        etaScaleGraph->SetTitle("Eta Scale");
+        etaScaleGraph->SetMarkerStyle(3);
+        etaScaleGraph->SetMarkerSize(2.5);
+        // etaScaleGraph->GetXaxis()->SetRangeUser(0, 20);
+        // etaScaleGraph->GetYaxis()->SetRangeUser(-0.5, 0.5);
+        // etaScaleGraph->Draw("A*");
 
-        
+        TGraph *etaResolutionGraph = new TGraph(fullEtaBins, eta, etaResolution);
+        etaResolutionGraph->GetXaxis()->SetTitle("Eta");
+        etaResolutionGraph->GetYaxis()->SetTitle("Resolution (RMS((reco-truth)/truth))"); 
+        etaResolutionGraph->SetTitle("Eta Resolution");
+        etaResolutionGraph->SetMarkerStyle(2);
+        etaResolutionGraph->SetMarkerColor(kRed);
+        etaResolutionGraph->SetMarkerSize(2.5);
+        // etaResolutionGraph->GetYaxis()->SetRangeUser(-0.1, 1);
+        // etaResolutionGraph->Draw("A* same");
+
+        TMultiGraph *etaMGraph = new TMultiGraph();
+        etaMGraph->Add(etaScaleGraph);
+        etaMGraph->Add(etaResolutionGraph);
+        etaMGraph->SetTitle("Eta Scale and Resolution");
+        etaMGraph->GetXaxis()->SetTitle("Eta");
+        etaMGraph->Draw("ap");
+
+        TLegend *etaLegend = new TLegend(0, 0);
+        etaLegend->AddEntry(etaScaleGraph);
+        etaLegend->AddEntry(etaResolutionGraph);
+        etaLegend->Draw();
+
         jetEnergy->cd(4);
+        TGraph *phiScaleGraph = new TGraph(fullPhiBins, phi, phiScale);
+        phiScaleGraph->GetXaxis()->SetTitle("phi");
+        phiScaleGraph->GetYaxis()->SetTitle("Scale (Mean((reco-truth)/truth))");
+        phiScaleGraph->SetTitle("Phi Scale");
+        phiScaleGraph->SetMarkerStyle(3);
+        phiScaleGraph->SetMarkerSize(2.5);
+        // phiScaleGraph->GetXaxis()->SetRangeUser(0, 20);
+        // phiScaleGraph->GetYaxis()->SetRangeUser(-0.5, 0.5);
+        // phiScaleGraph->Draw("A*");
 
-        TGraph *jetScale = new TGraph(fullBins, eta, scale);
-        jetScale->GetXaxis()->SetTitle("Eta");
-        jetScale->GetYaxis()->SetTitle("Scale (Mean((reco-truth)/truth))");
-        jetScale->SetTitle("Jet Scale");
-        jetScale->SetMarkerStyle(3);
-        jetScale->SetMarkerSize(2.5);
-        // jetScale->GetXaxis()->SetRangeUser(0, 20);
-        // jetScale->GetYaxis()->SetRangeUser(-0.5, 0.5);
-        // jetScale->Draw("A*");
+        TGraph *phiResolutionGraph = new TGraph(fullPhiBins, phi, phiResolution);
+        phiResolutionGraph->GetXaxis()->SetTitle("phi");
+        phiResolutionGraph->GetYaxis()->SetTitle("Resolution (RMS((reco-truth)/truth))"); 
+        phiResolutionGraph->SetTitle("Phi Resolution");
+        phiResolutionGraph->SetMarkerStyle(2);
+        phiResolutionGraph->SetMarkerColor(kRed);
+        phiResolutionGraph->SetMarkerSize(2.5);
+        // phiResolutionGraph->GetYaxis()->SetRangeUser(-0.1, 1);
+        // phiResolutionGraph->Draw("A* same");
 
-        TGraph *jetResolution = new TGraph(fullBins, eta, resolution);
-        jetResolution->GetXaxis()->SetTitle("Eta");
-        jetResolution->GetYaxis()->SetTitle("Resolution (RMS((reco-truth)/truth))"); 
-        jetResolution->SetTitle("Jet Resolution");
-        jetResolution->SetMarkerStyle(2);
-        jetResolution->SetMarkerColor(kRed);
-        jetResolution->SetMarkerSize(2.5);
-        // jetResolution->GetYaxis()->SetRangeUser(-0.1, 1);
-        // jetResolution->Draw("A* same");
+        TMultiGraph *phiMGraph = new TMultiGraph();
+        phiMGraph->Add(phiScaleGraph);
+        phiMGraph->Add(phiResolutionGraph);
+        phiMGraph->SetTitle("Phi Scale and Resolution");
+        phiMGraph->GetXaxis()->SetTitle("Phi");
+        phiMGraph->Draw("ap");
 
-        TMultiGraph *mGraph = new TMultiGraph();
-        mGraph->Add(jetScale);
-        mGraph->Add(jetResolution);
-        mGraph->Draw("ap");
-
-        TLegend *legend = new TLegend();
-        legend->AddEntry(jetScale);
-        legend->AddEntry(jetResolution);
-        legend->Draw();
+        TLegend *phiLegend = new TLegend();
+        phiLegend->AddEntry(phiScaleGraph);
+        phiLegend->AddEntry(phiResolutionGraph);
+        phiLegend->Draw();
 
 
     }
