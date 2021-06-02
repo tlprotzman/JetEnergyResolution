dca4c2ca8756067a10ca2117960fef7e76fcae33
Tristan Protzman <tlprotzman@gmail.com>
2021-06-02 10:26:26 -0400

    first pass at jet efficiency

---
+++ macro/jetEfficiency.cpp
@@ -0,0 +1,139 @@
+#include <TROOT.h>
+#include <TH1F.h>
+#include <TTree.h>
+#include <TFile.h>
+#include <TMath.h>
+#include <TCanvas.h>
+#include <TStyle.h>
+#include <TGraph.h>
+
+#include <list>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+// Binning
+const int num_bins = 50;
+const int min_energy = 0;
+const int max_energy = 50;
+
+// Cuts
+const float r = 0.5;
+
+// Shouldn't need to touch these
+const float r2 = r * r;
+
+// Translate file list into list of file paths
+// Really should make a personal library of functions...
+int readFileList(std::string fileList, std::list<std::string> &list) {
+    int numFiles = 0;
+    std::ifstream files(fileList);
+    std::string filePath;
+    
+    while (std::getline(files, filePath)) {
+        list.push_back(filePath);
+        numFiles++;
+    }
+    files.close();
+    return numFiles;
+}
+
+// pos = [truthEta, truthPhi, recoEta, recoPhi]
+// returns R2 = dEta * dEta + dPhi * dPhi
+float calculateDistance(float *pos) {
+    float dEta, dPhi;
+    dEta = pos[0] - pos[2];
+    dPhi = pos[1] - pos[3];
+    if (dPhi > TMath::Pi()) {
+        dPhi -= TMath::TwoPi();
+    }
+    if (dPhi < -1 * TMath::Pi()) {
+        dPhi += TMath::TwoPi();
+    }
+    return dEta *dEta + dPhi * dPhi;
+}
+
+void jetEfficiency(std::string jets = "") {
+    // Load files
+    std::list<std::string> filePaths;
+    std::cout << "read " << readFileList(jets, filePaths) << " file paths" << std::endl;
+
+    // 1D histograms to store number of truth jets and reco jets for each energy bin
+    TH1F *truthEnergy = new TH1F("truth_energy", "", num_bins, min_energy, max_energy);
+    TH1F *recoEnergy  = new TH1F("reco_energy",  "", num_bins, min_energy, max_energy);
+
+    // Loop over all the files
+    for (std::list<std::string>::iterator iter = filePaths.begin(); iter != filePaths.end(); ++iter) {
+        TFile *inFile = TFile::Open((*iter).c_str());       // open root file
+        if (inFile == nullptr) {
+            std::cerr << "Could not open file " << *iter << std::endl;
+            continue;
+        }
+        TTree *jets = (TTree*) inFile->Get("ntp_truthjet"); // get truthjet tree
+        if (jets == nullptr) {
+            std::cerr << "Could not file jet tree" << std::endl;
+            continue;
+        }
+
+        float truthE, recoE;
+        float pos[4];
+
+        jets->SetBranchAddress("ge", &truthE);
+        jets->SetBranchAddress("e", &recoE);
+        jets->SetBranchAddress("geta", &pos[0]);
+        jets->SetBranchAddress("gphi", &pos[1]);
+        jets->SetBranchAddress("eta", &pos[2]);
+        jets->SetBranchAddress("phi", &pos[3]);
+
+        for (uint32_t i = 0; i < jets->GetEntries(); i++) {
+            jets->GetEntry(i);
+            // Do we filter on R for efficiency? Probably
+            if (std::isnan(truthE) || std::isnan(recoE)) {
+                continue;
+            }
+            if (r2 < calculateDistance(pos)) {
+                continue;
+            }
+            truthEnergy->Fill(truthE);
+            recoEnergy->Fill(recoE);
+            // std::cout << truthE << "\t" << recoE << std::endl;
+            // std::cout << pos[0] << "\t" << pos[1] << std::endl;
+        }
+        inFile->Close();
+    }
+
+    // Calculate efficiencies
+    // efficiency = (num reco) / (num truth)
+    float *energy = (float*)malloc(num_bins * sizeof(float));
+    float *efficiency = (float*)malloc(num_bins * sizeof(float));
+    uint32_t fullBins = 0;
+    for (uint32_t i = 1; i < num_bins; i++) {
+        if (truthEnergy->GetBinContent(i) == 0) {
+            continue;
+        }
+        energy[fullBins] = truthEnergy->GetBinCenter(i);
+        efficiency[fullBins] = recoEnergy->GetBinContent(i) / truthEnergy->GetBinContent(i);
+        fullBins++;
+    }
+    std::cout << "filled " << fullBins << " bins" << std::endl;
+
+    // plotting
+    TCanvas *efficiencyCanvas = new TCanvas("jet_efficiency", "", 1000, 1000);
+    efficiencyCanvas->Divide(3, 1);
+
+    efficiencyCanvas->cd(1);
+    truthEnergy->Draw("colz");
+
+    efficiencyCanvas->cd(2);
+    recoEnergy->Draw("colz");
+
+    efficiencyCanvas->cd(3);
+    gPad->SetLeftMargin(0.15);
+    TGraph *efficiencyGraph = new TGraph(fullBins, energy, efficiency);
+    efficiencyGraph->SetTitle("Jet Efficiency");
+    efficiencyGraph->GetXaxis()->SetTitle("Jet Energy");
+    efficiencyGraph->GetYaxis()->SetTitle("Efficiency");
+    efficiencyGraph->Draw("A*");
+    gPad->SetLogy();
+    // efficiencyGraph->SaveAs("canvas.png");
+}
